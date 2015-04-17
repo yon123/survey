31e43e4227d7ff5d033303411afbc597c64ee509
Chad Centofante & Jonathan Aguilar <pair+chadcentofante.yon123@devbootcamp.com>
2015-04-16 17:02:34 -0700

    whatever

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,1 +1,20 @@
+get '/' do
+  @user = User.all
+  erb :"user/index"
+end
 
+get '/login' do
+  erb :"user/login"
+end
+
+post 'login' do
+
+end
+
+get '/signup' do
+  erb :"user/signup"
+end
+
+post '/signup' do
+
+end
